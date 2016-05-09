e728f0773c86cb9a320a1d01979c0ca241f78e31
jorge diaz <jorge diaz>
2016-05-09 09:36:18 -0500

    ajuste de .htaccess

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>cifoOiss</title>
+	<link rel="stylesheet" href="">
+</head>
+<style type="text/css">
+	body{
+		width: 100%;
+		height: 100%;
+		background: url('404.gif');
+		background-repeat: no-repeat;
+		background-color:#fff;
+		background-position: center 0;
+	}
+</style>
+<body>
+	
+</body>
+</html>
